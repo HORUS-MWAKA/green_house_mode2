a40a1d458da4b7fbcd17eb176b63d08d1625767d
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-02 01:19:41 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -64,7 +64,7 @@ if (selected == 'Aeration Prediction'):
     # creating a button for Prediction
     
     if st.button('Diabetes Test Result'):
-        diab_prediction = diabetes_model.predict([[Temperature, Humidity, Moisture, Watering])
+        diab_prediction = diabetes_model.predict([[Temperature, Humidity, Moisture, Watering]])
         
         if (diab_prediction[0] == 1):
           diab_diagnosis = 'Aeration Needed'
