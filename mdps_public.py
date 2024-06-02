124865e1a057c2f22b77e0175a2f5ba4e0c99982
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-02 17:13:26 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 21:01:15 2022
+
+@author: siddhardhan
+"""
+
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -5,7 +12,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+diabetes_model = pickle.load(open('logic_green_model.sav', 'rb'))
 
 heart_disease_model = pickle.load(open('logic_green_model.sav', 'rb'))
 
@@ -13,26 +20,93 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
-
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
     
     # page title
-st.title('Green House ML Model')
+    st.title('Diabetes Prediction using ML')
     
     
     # getting the input data from the user
-col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
-with col1:
-    Temperature = st.text_input('Temperature')
+    with col1:
+        Temperature = st.text_input('Temperature')
 
-with col2:
-    Humidity = st.text_input('Humidity')
+    with col2:
+        Humidity = st.text_input('Humidity')
 
-with col1:
-    Moisture = st.text_input('Moisture')
+    with col3:
+        Moisture = st.text_input('Moisture')
 
-with col2:
-    Watering = st.text_input('Watering')
+    with col1:
+        Watering = st.text_input('Watering')
     
     
+    # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Temperature, Humidity, Moisture, Watering]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
 
+
+
+
+# Heart Disease Prediction Page
+if (selected == 'Heart Disease Prediction'):
+    
+    # page title
+    st.title('Heart Disease Prediction using ML')
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Temperature = st.text_input('Temperature')
+
+    with col2:
+        Humidity = st.text_input('Humidity')
+
+    with col3:
+        Moisture = st.text_input('Moisture')
+
+    with col1:
+        Watering = st.text_input('Aeration')
+        
+        
+     
+     
+    # code for Prediction
+    heart_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = heart_disease_model.predict([[Temperature, Humidity, Moisture, Aeration]]])                          
+        
+        if (heart_prediction[0] == 1):
+          heart_diagnosis = 'The person is having heart disease'
+        else:
+          heart_diagnosis = 'The person does not have any heart disease'
+        
+    st.success(heart_diagnosis)
