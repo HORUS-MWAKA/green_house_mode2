402668d5aa6187972eb1fbd709a1e5f1d412a69b
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-02 16:14:34 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,46 +1,64 @@
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-import joblib
 
-def show_entry_fields():
-    p1=float(e1.get())
-    p2=int(e2.get())
-    p3=float(e3.get())
-    p4=int(e4.get())
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('mode_aeration_saved', 'rb'))
+
+heart_disease_model = pickle.load(open('mode_watering_saved', 'rb'))
+
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+
+
+
+# sidebar for navigation
+with st.sidebar:
     
-    model = joblib.load('model_greenhouse')
-    result=model.predict([[p1,p2,p3,p4]])
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
 
-    if result == 0:
-        Label(master, text="No Watering").grid(row=6)
+    with col1:
+        Temperature = st.text_input('Temperature')
 
-    else:
-        Label(master, text="Garden Waterig Needed").grid(row=6)
+    with col2:
+        Humidity = st.text_input('Humidity')
 
-#master = Tk()
-#master.title("Greenhouse Predictive Model")
-st.title('Greenhouse Predictive Model')
+    with col3:
+        Moisture = st.text_input('Moisture')
 
-label = Label(st.text = "Greenhouse Predictive Model"
-               , bg = "black", fg = "white"). \
-                    grid(row=0,columnspan=2)
-
-Label(master, text="Enter Temperature Value").grid(row=1)
-Label(master, text="Enter Humidity Value").grid(row=2)
-Label(master, text="Enter Soil Moisture Value").grid(row=3)
-Label(master, text="Enter Aeration State Value").grid(row=4)
-
-e1=Entry(master)
-e2=Entry(master)
-e3=Entry(master)
-e4=Entry(master)
-
-e1.grid(row=1,column=1)
-e2.grid(row=2,column=1)
-e3.grid(row=3,column=1)
-e4.grid(row=4,column=1)
-
-Button(master,text='Predict', command=show_entry_fields).grid()
-
-#mainloop()
+    with col1:
+        Watering = st.text_input('Watering')
+    
+    
+    # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Temperature, Humidity, Moisture, Watering]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
