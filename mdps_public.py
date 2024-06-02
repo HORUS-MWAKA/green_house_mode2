a97759a31407f9aa2d690c016221de4b25bda458
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-02 20:17:37 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,9 +12,9 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('logic_green_model.sav', 'rb'))
+diabetes_model = pickle.load(open('mode_watering_saved.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('logic_green_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('mode_aeration_saved.sav', 'rb'))
 
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
